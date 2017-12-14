44fbd85c82049963d688f7148a19e58bfc23fc59
maxmyth01 <maxmyth01@gmail.com>
2017-12-14 14:43:48 -0500

    Updated from Brython Server: 12/14/2017 2:43:47 PM

--- battleship.py
+++ battleship.py
@@ -1,1 +1,22 @@
+#Max Low
+#12-14-17
+#battleship.py -- battleship game
 
+def buildBoard():
+    board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+    for row in range(0,5):
+        for col in range(0,5):
+            print(board[row][col],' ',end = '')
+        print()
+"""
+def redrawAll():
+    
+def pickComputerShips():
+    
+def computerTurn():
+    
+def mouseClick():
+"""
+    
+if __name__ == '__main__':
+    buildBoard()
