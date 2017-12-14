3fa8b0cf8222950de5d717382cbe6738c0fea7cd
maxmyth01 <maxmyth01@gmail.com>
2017-12-14 14:47:57 -0500

    Updated from Brython Server: 12/14/2017 2:47:57 PM

--- battleship.py
+++ battleship.py
@@ -2,11 +2,19 @@
 #12-14-17
 #battleship.py -- battleship game
 
+#constants
+ROWS = 5
+COLS = 5
+CELL_SIZE = 20
+black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
+
 def buildBoard():
+    tile = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black),white)
     board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
     for row in range(0,5):
         for col in range(0,5):
-            print(board[row][col],' ',end = '')
+            print(board[row][col],' :',end = '')
         print()
 """
 def redrawAll():
