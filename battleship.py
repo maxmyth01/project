9e71ce402b743df3615329375782b12df4e0ba42
maxmyth01 <maxmyth01@gmail.com>
2017-12-14 14:57:19 -0500

    Updated from Brython Server: 12/14/2017 2:57:18 PM

--- battleship.py
+++ battleship.py
@@ -2,6 +2,8 @@
 #12-14-17
 #battleship.py -- battleship game
 
+from ggame import *
+
  #constants and colors
 ROWS = 5
 COLS = 5
@@ -11,13 +13,14 @@ white = Color(0xFFFFFF,1)
 
 def buildBoard():
     board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-    square = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),white)
-    for row in range(0,5):
+
+"""
+def redrawAll():
+square = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),white)
+ for row in range(0,5):
         for col in range(0,5):
             print(board[row][col],' ',end = '')
         print()
-"""
-def redrawAll():
     
 def pickComputerShips():
     
