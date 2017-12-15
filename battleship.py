7ac2920e7147f354411f8c58370bf7bd7b8c98de
maxmyth01 <maxmyth01@gmail.com>
2017-12-15 14:46:41 -0500

    Updated from Brython Server: 12/15/2017 2:46:40 PM

--- battleship.py
+++ battleship.py
@@ -3,10 +3,11 @@
 #battleship.py -- battleship game
 
 from ggame import *
+from random import randint
 
  #constants and colors
-ROWS = 5
-COLS = 5
+ROWS = 3
+COLS = 12
 CELL_SIZE = 20
 black = Color(0x000000,1)
 white = Color(0xFFFFFF,1)
@@ -22,17 +23,22 @@ def buildBoard():
 
 
 def redrawAll():
+    for item in App().spritelist[:]:
+        item.destroy()
+    
     y=0
-    square = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black ),white)
+    square = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black),white)
     for row in range(0,ROWS):
         x=0
         for col in range(0,COLS):
             Sprite(square,(x,y))
             x += CELL_SIZE
         y += CELL_SIZE
-"""
+
 def pickComputerShips():
+    board[randint(0,ROWS)][randint(0,COLS)] = X 
     
+"""
 def computerTurn():
     
 def mouseClick():
